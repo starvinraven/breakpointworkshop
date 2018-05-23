3c4972712e3e7b1c31eb5a20ec5668f4cef26156
Esko Lahti <esko.lahti@reaktor.com>
2018-05-23 21:59:19 +0300

    Don't wobble, thanks a lot

--- src/cljs/breakpoint_app/animation/animation_core.cljs
+++ src/cljs/breakpoint_app/animation/animation_core.cljs
@@ -3,8 +3,12 @@
             [re-frame.core :as re-frame])
   (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
+(defonce animation-loop-running? (atom false))
+
 (defn start-animation-loop []
-  (go-loop []
-    (async/<! (async/timeout 3000))
-    (re-frame/dispatch [:animation/toggle-animation-state])
-    (recur)))
+  (when-not @animation-loop-running?
+    (reset! animation-loop-running? true)
+    (go-loop []
+      (async/<! (async/timeout 3000))
+      (re-frame/dispatch [:animation/toggle-animation-state])
+      (recur))))
